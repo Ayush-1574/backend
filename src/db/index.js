bf98159495eb43e531e1b61610adc033ec71777f
Ayush Sonika <2023csb1107@iitrpr.ac.in>
2025-06-26 22:04:54 +0530

    Logout functionality added

--- src/db/index.js
+++ src/db/index.js
@@ -1,20 +1,14 @@
 import express from "express"
 import mongoose from "mongoose"
 import {DB_NAME} from "../constants.js"
-const app = express()
+
 
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        await mongoose.connect(`${process.env.MONGO_URL}${DB_NAME}`)
         console.log("Connected to MongoDB")
-        app.on("error" , (error) => {
-            console.log("ERR: " , error);
-            throw error
-        })
-        app.listen(process.env.PORT || 3000 , () =>{
-            console.log("APP is listing on Port" , process.env.PORT )
-        })
+       
     }
     catch(error){
         console.error("ERROR ", error)
